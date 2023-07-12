ff8244ac1caf4137868bb6d7e48937e60cb42352
Yuri Gabriel <yurigabriel1995@outlook.com>
2023-07-11 23:11:45 -0300

    GET game by id

--- src/main/java/com/yuri/dslist/controllers/GameController.java
+++ src/main/java/com/yuri/dslist/controllers/GameController.java
@@ -4,9 +4,11 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.yuri.dslist.dto.GameDTO;
 import com.yuri.dslist.dto.GameMinDTO;
 import com.yuri.dslist.entities.Game;
 import com.yuri.dslist.services.GameService;
@@ -17,6 +19,12 @@ public class GameController {
 	
 	@Autowired
 	private GameService gameService;
+	
+	@GetMapping(value = "/{id}")
+	public GameDTO findById(@PathVariable Long id) {
+		GameDTO result = gameService.findById(id);
+		return result;
+	}
 	
 	@GetMapping
 	public List<GameMinDTO> findAll() {
