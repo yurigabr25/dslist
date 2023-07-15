2e33b8936e31263f7a3ad70b7643f97dfb18298a
Yuri Gabriel <yurigabriel1995@outlook.com>
2023-07-15 02:59:14 -0300

    Move game on list

--- src/main/java/com/yuri/dslist/services/GameListService.java
+++ src/main/java/com/yuri/dslist/services/GameListService.java
@@ -8,17 +8,37 @@ import org.springframework.transaction.annotation.Transactional;
 
 import com.yuri.dslist.dto.GameListDTO;
 import com.yuri.dslist.entities.GameList;
+import com.yuri.dslist.projections.GameMinProjection;
 import com.yuri.dslist.repositories.GameListRepository;
+import com.yuri.dslist.repositories.GameRepository;
 
 @Service
 public class GameListService {
 
 	@Autowired
 	private GameListRepository gameListRepository;
+	
+	@Autowired
+	private GameRepository gameRepository;
 
 	@Transactional(readOnly = true)
 	public List<GameListDTO> findAll() {
 		List<GameList> result = gameListRepository.findAll();
 		return result.stream().map(x -> new GameListDTO(x)).toList();
 	}
+	
+	@Transactional
+	public void move(Long listId, int sourceIndex, int destinationIndex) {
+		List<GameMinProjection>  list = gameRepository.searchByList(listId);
+		GameMinProjection obj = list.remove(sourceIndex);
+		list.add(destinationIndex, obj);
+		
+		int min = sourceIndex < destinationIndex ? sourceIndex : destinationIndex;
+		int max = sourceIndex < destinationIndex ? destinationIndex : sourceIndex;
+		
+		for (int i = min; i <= max; i++) {
+			gameListRepository.updateBelongingPosition(listId, list.get(i).getId(), i);
+		}
+	}
+	
 }
